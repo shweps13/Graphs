6c19cc3ef96732d750128f3f5cc3e3208d7bcb56
Heorhii Siburov <g.siburov@gmail.com>
2020-02-19 14:00:10 -0800

    class tasks

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,10 @@
+import random
+
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
@@ -45,8 +49,31 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i+1}")
 
         # Create friendships
+        # create a list with all possible friendships
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+
+        # Shuffle the list
+        random.shuffle(possible_friendships)
+        print("----")
+        print(possible_friendships)
+        print("----")
+        # Grab the first N pairs from the list and create those friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+
+        # avg_friendships = total_friendships / num_users
+        # total_friendships = avg_friendships * num_users
+        # N = avg_friendships * num_users // 2
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -65,6 +92,7 @@ class SocialGraph:
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
+    print(sg.users)
     print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+    # connections = sg.get_all_social_paths(1)
+    # print(connections)
