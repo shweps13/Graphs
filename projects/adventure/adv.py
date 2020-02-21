c325bd73d570c49b55a4533baac569fba110df3f
Heorhii Siburov <g.siburov@gmail.com>
2020-02-21 10:20:40 -0800

    looks like mvp done

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -12,9 +12,9 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -95,40 +95,46 @@ def bfs(starting_room):
 
 def search(starting_room):
     print("===Beginning of search===\n")
-    visited_room = set()
-
-    # Where player now
-    current_room = player.current_room
-    
-    # Id of current room
-    room_id = current_room.id
+    # Counter of rooms where player was
+    visited_room = 0
 
     # Define opposite directions
     global_directions = {'n':'s', 's':'n', 'e':'w', 'w':'e'}
 
-    # Define dictionary with room directions
-    room_dict = {}
-
     while len(local_graph) != len(room_graph):
         # print("current room:", room_id)
+        
+        # Where player now
+        current_room = player.current_room
+
+        # # Id of current room
+        room_id = current_room.id
+        
+        # Define dictionary with room directions
+        room_dict = {}
 
         if room_id not in local_graph:
             # Find possible exits
 
             for i in current_room.get_exits():
+                # add "?" to the room dictionary
                 room_dict[i] = '?'
                 # print("room dictionary", room_dict) 
 
             if traversal_path:
+                # previous room == opposide direction of last traversal path
                 previos_room = global_directions[traversal_path[-1]]
-                room_dict[previos_room] = previos_room
+
+                # add previous_room to the room dictionaty
+                room_dict[previos_room] = visited_room
 
             # add unexplored room to the room id
             local_graph[room_id] = room_dict
-            print("Local graph: ", local_graph[room_id])
+            # print("Local graph: ", local_graph[room_id])
 
         else:
-            break
+            # Add the room id from local_graph to the inner room dictionary
+            room_dict = local_graph[room_id]
         
         # need to see if a room connected
         possible_exits = []
@@ -147,12 +153,11 @@ def search(starting_room):
         if len(possible_exits) != 0:
             
             random.shuffle(possible_exits)
-            print("next possible direction to go ===>", possible_exits[0])
+            print("next possible direction to go ===> ['", possible_exits[0], "']")
 
             direction = possible_exits[0]
-            # move player to direction
             
-            # log the direction
+            # log the direction to traversal
             traversal_path.append(direction)
             # print('traversal path', traversal_path)
 
@@ -161,23 +166,63 @@ def search(starting_room):
             # print("we moved to ", direction)
             # print("current room", room_id)
 
-            for movement in traversal_path:
-                player.travel(movement)
-                print("movement ['", movement, "']")
-                movement = player.current_room
-                print("current room:", movement.id)
-                print("possible to go:", player.current_room.get_exits())
-                exits = player.current_room.get_exits()
-                print("exit to", exits[0])
+            # for movement in traversal_path:
+            #     player.travel(movement)
+            #     print("movement ['", movement, "']")
+            #     movement = player.current_room
+            #     print("current room:", movement.id)
+            #     print("possible to go:", player.current_room.get_exits())
+            #     exits = player.current_room.get_exits()
+            #     print("exit to", exits[0])
 
-            # log to traversal path
-            traversal_path.append(exits[0])
+            # # log to traversal path
+            # traversal_path.append(exits[0])
+
+            # move player to direction
+            player.travel(direction)
+
+            # replace "?" in local_graph with discovered rroms
+            movement = player.current_room
+            # print("room", local_graph[current_room.id][direction])
+            local_graph[current_room.id][direction] = movement.id
+            # print("movement", movement.id)
+            visited_room = current_room.id
+            # print('visited room id: ', visited_room)
+
         else:
+            # BFS to search for next exits/possible rooms using room_id
             next_room = bfs(room_id)
-            print("using bfs for next room",next_room)
+            # print("Next room", next_room) 
+            # print("length of next_room", len(next_room))
+
+            # if path of next_room has results from bfs
+            if next_room is not None and len(next_room) > 0:
+                # print("===Testtestestest===")
+                
+                # iterate length of the room to give access ro room id
+                for i in range(len(next_room) -1):
+                    # print("map check at: ", local_graph[next_room[i]])
+
+                    # iterate the local_graph dictionary next room at this index to get directions
+                    for direction in local_graph[next_room[i]]:
+                        # print("direction", direction)
+                        # print("conditional", local_graph[next_room[i]][direction])
+                        # print("checking next plus one", next_room[i + 1])
+
+                        # if local_graph next_room[i] and [direction] matches that of following room[i] found thru bfs
+                        if local_graph[next_room[i]][direction]  == next_room[i + 1]:
+                            # print("traversal_path", traversal_path.append(direction))
+                            
+                            # append the direction to travel_path
+                            traversal_path.append(direction)
+                            # print("player travel", traversal_path.append(direction))
+                            # move player to that room
+                            player.travel(direction)
+                            # print("player travel", current_room.id)
+            else:
+                break
 
             
-
         
 
 
